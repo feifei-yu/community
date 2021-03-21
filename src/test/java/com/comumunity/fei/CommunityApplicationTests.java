2ce5690342ee4e0295b60996db5dd9d3515d45bc
余菲 <yufei@yufeideMacBook-Pro.local>
2021-03-21 19:22:29 +0800

    init repo

---
+++ src/test/java/com/comumunity/fei/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comumunity.fei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
